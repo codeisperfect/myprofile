54f9a35aa0135b04fe55abe1357a07ba49ecae6a
Mohit <mohitsaini1196@gmail.com>
2016-07-30 00:38:14 +0530

    new profile

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ function disp_tabs($isbig=true) {
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-	<title>Parallax Template - Materialize</title>
+	<title>StillHungry | Chewing each byte of code</title>
 
 	<!-- CSS  -->
 <!-- 	<link href="assets/css/gfont.css" rel="stylesheet">
